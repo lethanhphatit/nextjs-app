b6943fbe0b006155a38ad13e6bdf3e22c2041999
Phat le <phat.le.set14@eiu.edu.vn>
2022-06-29 01:52:34 +0700

    update meta tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
