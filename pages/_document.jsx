ab6f1311f6e55ca23f51e2c8ee6c4f2bb8b21ea5
Phat le <phat.le.set14@eiu.edu.vn>
2022-06-29 09:54:43 +0700

    update seo

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:url" content="https://food-store-next-js.vercel.app/" />
         </Head>
         <body>
           <Main />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,29 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <main>
       <Head>
-        <title>Burger Queen Combo 1</title>
-        <meta name="description" content="Zing burger queen combo 1 - Zing Food" />
+        <title>BURGER KING MENU</title>
+        <meta name="description" content="Zing burger king menu - Zing Food" />
       </Head>
-      <h1>Burger Queen Combo 1</h1>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-XXXXX-Y', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+      <h1>BURGER KING MENU</h1>
       <img
-        alt="Burger Queen Combo 1"
+        alt="burger king menu"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUAOQs4EuR-qmZtp4UbiOOeapFF9cp6e2WkQ&usqp=CAU&fit=crop&w=620h=413"
       />
     </main>
