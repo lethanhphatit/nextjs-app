166347c0d2b7127e7b9c284af8b817a80e9cf038
Phat le <phat.le.set14@eiu.edu.vn>
2022-06-29 09:58:24 +0700

    update new (1)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta property="og:url" content="https://food-store-next-js.vercel.app/" />
+          <meta property="og:url" content="https://burger-king-menu.vercel.app/" />
         </Head>
         <body>
           <Main />
